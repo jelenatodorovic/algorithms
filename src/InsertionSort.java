d7917dfe2b8aa278ad8f8b7d00312368226cd7fe
Jelena <jelena.t12@gmail.com>
2019-04-14 18:27:43 +0200

    Insertion sort fix

--- src/InsertionSort.java
+++ src/InsertionSort.java
@@ -3,29 +3,27 @@
  */
 public class InsertionSort {
 
-  public static void main(String[] args) {
-    int[] a = {5, 2, 9, 6};
-    int[] b = insertionSort(a);
-    for (int i = 0; i < b.length; i++) {
-      System.out.println(b[i]);
-    }
-
-  }
-
-  public static int[] insertionSort(int[] a) {
-
-    for (int i = 1; i < a.length; i++) {
-      int key = a[i];
-      int j = 0;
-      while (j < i) {
-        if (key < a[j]) {
-          a[i] = a[j];
-          a[j] = key;
+    public static void main(String[] args) {
+        int[] a = {5, 2, 9, 6};
+        int[] b = insertionSort(a);
+        for (int i = 0; i < b.length; i++) {
+            System.out.println(b[i]);
         }
-        j++;
-      }
+
     }
-    return a;
-  }
+
+    public static int[] insertionSort(int[] a) {
+
+        for (int i = 1; i < a.length; i++) {
+            int key = a[i];
+            int j = i - 1;
+            while (j >= 0 && key < a[j]) {
+                a[j + 1] = a[j];
+                j--;
+            }
+            a[j + 1] = key;
+        }
+        return a;
+    }
 
 }
